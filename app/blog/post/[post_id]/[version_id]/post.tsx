fc974ee1d1e78f33dcf2c8ae546b5835aa11c1d0
jakenichols2719 <jnichols2719@protonmail.com>
2023-01-06 16:31:15 -0700

    No grey background on blog posts

--- app/blog/post/[post_id]/[version_id]/post.tsx
+++ app/blog/post/[post_id]/[version_id]/post.tsx
@@ -3,7 +3,7 @@ export default function BlogPost(
     {content: string}
 ) {
     return (
-        <div className="w-9/12 py-10 my-5 flex justify-center border-4 border-primary shadow-md bg-base-200">
+        <div className="w-9/12 py-10 my-5 flex justify-center border-4 border-primary shadow-md bg-base-100">
              <article className="sm:prose-sm md:prose lg:prose-xl"
             dangerouslySetInnerHTML={{"__html": content}}>
             </article>
