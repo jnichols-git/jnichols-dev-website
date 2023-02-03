cfccfe1fd6125556e62bf16886cb619bf04077b2
jakenichols2719 <jnichols2719@protonmail.com>
2023-02-03 15:18:10 -0700

    Rendering change in blogposts and removal of requirement for max-vh pages

--- app/blog/post/[post_id]/[version_id]/post.tsx
+++ app/blog/post/[post_id]/[version_id]/post.tsx
@@ -3,7 +3,7 @@ export default function BlogPost(
     {content: string}
 ) {
     return (
-        <article className="py-10 mx-6 prose-sm max-w-none md:prose md:max-w-prose lg:prose-xl"
+        <article className="py-10 mx-6 prose prose-sm max-w-none lg:prose-xl lg:max-w-prose"
             dangerouslySetInnerHTML={{"__html": content}}>
         </article>
     )
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,10 @@ export default function RootLayout(
 			<body data-theme="j">
         <div className="navbar bg-base-100 rounded-box">
           <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <label tabIndex={0} className="btn btn-primary lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
-            <ul tabIndex={0} className="menu dropdown-content mt-3 p-2 shadow bg-base-200 rounded-box w-52">
+            <ul tabIndex={0} className="menu bg-primary text-primary-content dropdown-content mt-3 p-2 shadow bg-base-200 rounded-box w-52">
               {NavMenu()}
             </ul>
           </div>
